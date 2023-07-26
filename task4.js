93ae4f6929efab2d7e58dcbef066561f167b3aec
Assala Meslmani <139454177+assalameslmani@users.noreply.github.com>
2023-07-26 11:01:41 +0300

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,15 @@
+let fruits = ['apple','banana','orange'];
+console.log(fruits[0]);//output:'apple'
+console.log(fruits[1]);//output:'banana'
+console.log(fruits[2]);//output:'orange'
+
+let numbers = ['5','8','6'];
+console.log(numbers[0]);//output:'5'
+console.log(numbers[1]);//output:'8'
+console.log(numbers[2]);//output:'6'
+
+let mixedarray = ['apple',1 , true, {name: 'jhon'}];
+console.log(mixedarray[0]);//output:'apple'
+console.log(mixedarray[1]);//output:1
+console.log(mixedarray[2]);//output:true
+console.log(mixedarray[3]);//output:{name: 'jhon'}
