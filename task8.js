1f354ca9a5e46fdeba773ab9d9f0c1026f928c0a
Assala Meslmani <139454177+assalameslmani@users.noreply.github.com>
2023-07-27 10:53:48 +0300

    Add files via upload

---
+++ task8.js
@@ -0,0 +1,6 @@
+var randomNumber = 0;
+while (randomNumber <=8){
+    randomNumber= Math.floor(Math.random()*10)+1;
+    console.log (randomNumber);
+}
+console.log("Random number greater than 8 generated!")
