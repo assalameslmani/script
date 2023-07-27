6f8f356d6ce560acadc4b91043ec1f0a2bfe7b17
Assala Meslmani <139454177+assalameslmani@users.noreply.github.com>
2023-07-27 12:31:46 +0300

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,23 @@
+function myFunction1() {
+    alert("message1");
+}
+
+function myFunction2() {
+    alert("message2");
+}
+
+function myFunction3() {
+    alert("message3");
+}
+
+function myFunction4() {
+    alert("message4");
+}
+
+function myFunction5() {
+    alert("message5");
+}
+
+function myFunction6() {
+    alert("Submit done");
+}
